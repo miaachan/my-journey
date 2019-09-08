1a4bba6a36f9963f25fe870184414ca6e38ecc79
miaachan <kimyeungg@outlook.com>
2019-09-08 12:05:45 +0800

    up

---
+++ 6kyu/6kyu - Don't rely on luck.js
@@ -0,0 +1,8 @@
+/*
+        6kyu - Don't rely on luck.
+        https://www.codewars.com/kata/dont-rely-on-luck/train/javascript
+
+*/
+
+Math.floor = function () {return "Stupid kata.";}
+var guess = Math.floor();
