b0b6c3cc01440a28f17b2592d32cef8011d1cc94
miaachan <kimyeungg@outlook.com>
2019-10-18 05:32:56 +0800

    up reg kata

---
+++ 7kyu/7kyu - Training JS #40 Regular Expression.js
@@ -0,0 +1,6 @@
+/*
+        7kyu - Training JS #40: Regular Expression--"|", "[]" and "()"
+        https://www.codewars.com/kata/training-js-number-40-regular-expression-and/train/javascript
+*/
+
+let regex = /https?:\/\/[a-z0-9.]+\.(com|net)/ig
