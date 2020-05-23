11bbb2d20129b718b6a9462bf9a8a4c1ad286fca
miaachan <kimyeungg@outlook.com>
2020-05-24 03:58:17 +0800

    i love hong kong

--- Leetcode/Easy/160. Intersection of Two Linked Lists.js
+++ Leetcode/Easy/160. Intersection of Two Linked Lists.js
@@ -11,6 +11,60 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-var getIntersectionNode = function(headA, headB) {
-    
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+
+/* 
+   Two pointer method
+   Time complexity : O(m+n)
+   Space complexity : O(1)
+*/
+var getIntersectionNode = function (headA, headB) {
+    let ptrA = headA,
+        ptrB = headB;
+
+    while (ptrA != ptrB) {
+        ptrA = (ptrA) ? ptrA.next : headB;
+        ptrB = (ptrB) ? ptrB.next : headA;
+    }
+    return ptrA;
 };
+
+
+/*
+    Time complexity : O(m+n)
+    Space complexity : O(m) or O(n).
+
+var getIntersectionNode = function (headA, headB) {
+    let set = new Set();
+    while (headA || headB) {
+        if (headA == headB) {
+            return headA;
+        }
+        if (set.has(headA)) {
+            return headA;
+        } else if (set.has(headB)) {
+            return headB;
+        }
+        else {
+            if (headA) set.add(headA);
+            if (headB) set.add(headB);
+        }
+        headA = (headA) ? headA.next : null;
+        headB = (headB) ? headB.next : null;
+    }
+    return null;
+};
+*/
