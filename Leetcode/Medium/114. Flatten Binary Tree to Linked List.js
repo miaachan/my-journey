dc33d58a2f3958ea3d05bbe74d57910a6075d615
miaachan <kimyeungg@outlook.com>
2020-09-01 20:46:51 +0800

    improve M114

--- Leetcode/Medium/114. Flatten Binary Tree to Linked List.js
+++ Leetcode/Medium/114. Flatten Binary Tree to Linked List.js
@@ -10,30 +10,68 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+
 var flatten = function (root) {
     /**
-     * pre-order dfs put all node into an Array
-     * modify the original root after dfs
+     * post-order dfs
+     *
+     *
+     *     1            1           1
+     *    / \          /             \
+     *   2   3        2               2
+     *                 \               \
+     *                  3               3
+     *
+     * Time O(n); Space: O(h) h = height of the tree
      */
     if (root === null) return;
-    let preorder = [];
 
     dfs(root);
-    // remove the first one as it equals to root.val
-    preorder.shift();
-    root.left = null;
-    while (preorder.length > 0) {
-        root.right = new TreeNode(preorder.shift(), null, null);
-        root = root.right;
-    }
+    console.log(JSON.stringify(root));
 
     function dfs(node) {
         if (node === null) return;
-        preorder.push(node.val);
+        // preorder.push(node.val);
         dfs(node.left);
         dfs(node.right);
+        if (node.left !== null) {
+            let condom = node.left;
+            while (condom.right !== null) {
+                condom = condom.right;
+            }
+            condom.right = node.right;
+            node.right = node.left;
+            node.left = null;
+        }
     }
 };
+
+// var flatten = function (root) {
+//     /**
+//      * Time O(n); Space O(n)
+//      *
+//      * pre-order dfs put all node into an Array
+//      * modify the original root after dfs
+//      */
+//     if (root === null) return;
+//     let preorder = [];
+
+//     dfs(root);
+//     // remove the first one as it equals to root.val
+//     preorder.shift();
+//     root.left = null;
+//     while (preorder.length > 0) {
+//         root.right = new TreeNode(preorder.shift(), null, null);
+//         root = root.right;
+//     }
+
+//     function dfs(node) {
+//         if (node === null) return;
+//         preorder.push(node.val);
+//         dfs(node.left);
+//         dfs(node.right);
+//     }
+// };
 
 flatten({
     val: 1,
