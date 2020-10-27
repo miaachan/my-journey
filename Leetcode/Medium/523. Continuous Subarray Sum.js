8ba12542fed6afaabd0cc3839805f19af55287ed
miaachan <kimyeungg@outlook.com>
2020-10-27 13:46:48 +0800

    save

--- Leetcode/Medium/523. Continuous Subarray Sum.js
+++ Leetcode/Medium/523. Continuous Subarray Sum.js
@@ -4,11 +4,21 @@
  * @return {boolean}
  */
 var checkSubarraySum = function (nums, k) {
-    let max = nums.reduce((sum, cur) => sum + cur);
-    let dp = Array.from(Array(nums.length), () => Array(max + 1).fill(false));
-    console.log(dp.length);
-    dp[1][2] = true;
-    console.log(dp);
+    let sum = nums.reduce((sum, cur) => sum + cur);
+
+    let dp = Array.from(Array(nums.length + 1), () =>
+        Array(sum + 1).fill(false)
+    );
+
+
+    // can pick or not-to-pick
+    for (let i = 0; i <= nums.length; ++i) {
+        dp[i][0] = true;
+    }
+
+
+
+
 };
 
-checkSubarraySum([23, 2, 4, 6, 7]);
+// checkSubarraySum([23, 2, 6, 4, 7], 6);
