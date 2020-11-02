4e0db1a16bb910e83be4c02d2b99ff12c657a7bc
miaachan <kimyeungg@outlook.com>
2020-11-02 23:44:44 +0800

    save

--- Leetcode/Medium/147. Insertion Sort List.ts
+++ Leetcode/Medium/147. Insertion Sort List.ts
@@ -1,16 +1,4 @@
-
-
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import { ListNode, ListNodeBuilder } from "../lib";
 
 function insertionSortList(head: ListNode | null): ListNode | null {
     console.log(head);
