264f1a01eb4b7837e9fb3f019c178dfb6e161dc5
miaachan <kimyeungg@outlook.com>
2020-11-02 23:46:47 +0800

    pq todo

--- Leetcode/lib.ts
+++ Leetcode/lib.ts
@@ -20,3 +20,15 @@ export function ListNodeBuilder(nodes: number[]): ListNode {
     });
     return dHead.next;
 }
+
+
+
+class PriorityQueue {
+    constructor() {}
+
+    parent = (i) => ((i + 1) >>> 1) - 1; // Math.floor((index - 1) / 2)
+    left = (i) => (i << 1) + 1; // i * 2 + 1
+    right = (i) => (i + 1) << 1; // i * 2 + 2
+}
+
+let minHeap = new PriorityQueue();
--- Leetcode/mylib.ts
+++
@@ -1,9 +0,0 @@
-class PriorityQueue {
-    constructor() {}
-
-    parent = (i) => ((i + 1) >>> 1) - 1; // Math.floor((index - 1) / 2)
-    left = (i) => (i << 1) + 1; // i * 2 + 1
-    right = (i) => (i + 1) << 1; // i * 2 + 2
-}
-
-let minHeap = new PriorityQueue();
