3dd0dfcb73c4f9d88d49c802f7b589156f752dea
miaachan <kimyeungg@outlook.com>
2020-11-03 15:21:31 +0800

    save 147

--- Leetcode/Medium/147. Insertion Sort List.ts
+++ Leetcode/Medium/147. Insertion Sort List.ts
@@ -1,8 +1,23 @@
 import { ListNode, ListNodeBuilder } from "../lib";
 
 function insertionSortList(head: ListNode | null): ListNode | null {
-    console.log(head);
-    return null;
+    let dummy = new ListNode(0);
+    let prev = dummy;
+    // need to maintain a linked-list for Backward reference
+    while (head != null) {
+        let temp = head.next;
+
+        if (prev.val >= head.val) prev = dummy;
+        while (prev.next !== null && prev.next.val < head.val) {
+            prev = prev.next;
+        }
+        head.next = prev.next;
+        prev.next = head;
+
+        head = temp;
+    }
+
+    return dummy.next;
 }
 
 insertionSortList(ListNodeBuilder([4, 2, 1, 3]));
