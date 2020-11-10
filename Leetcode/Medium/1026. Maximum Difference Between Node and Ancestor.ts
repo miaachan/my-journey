d20b18775b1d31338ff78a6007d60c671bad2327
miaachan <kimyeungg@outlook.com>
2020-11-10 18:20:43 +0800

    M1026: dfs

--- Leetcode/Medium/1026. Maximum Difference Between Node and Ancestor.ts
+++ Leetcode/Medium/1026. Maximum Difference Between Node and Ancestor.ts
@@ -1,39 +1,16 @@
-// my on9 dfs
+// dfs
 function maxAncestorDiff(root: TreeNode | null): number {
     let maxDiff = 0;
-    dfs(root);
+    dfs(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
     return maxDiff;
 
-    function dfs(
-        node: TreeNode | null
-    ): {
-        min: number;
-        max: number;
-    } {
-        if (node === null)
-            return {
-                min: Number.MAX_SAFE_INTEGER,
-                max: Number.MIN_SAFE_INTEGER,
-            };
-
-        // for node doesnt have any childs, return its own.
-        if (node.left === null && node.right === null)
-            return { min: node.val, max: node.val };
-
-        let left = dfs(node.left);
-        let right = dfs(node.right);
-
-        let min = Math.min(left.min, right.min);
-        let max = Math.max(left.max, right.max);
-
-        let curDiff = Math.max(
-            Math.abs(node.val - min),
-            Math.abs(node.val - max)
-        );
-
-        maxDiff = Math.max(maxDiff, curDiff);
-
-        return { min: Math.min(node.val, min), max: Math.max(node.val, max) };
+    function dfs(node: TreeNode | null, max: number, min: number): void {
+        if (node === null) return;
+        max = Math.max(node.val, max);
+        min = Math.min(node.val, min);
+        maxDiff = Math.max(maxDiff, max - min);
+        dfs(node.left, max, min);
+        dfs(node.right, max, min);
     }
 }
 
