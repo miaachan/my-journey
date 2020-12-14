f9a72978181b18c11b98a565564588b5f6e2f86b
miaachan <kimyeungg@outlook.com>
2020-12-14 23:12:26 +0800

    lib: lc treenode builder

--- Leetcode/lib.ts
+++ Leetcode/lib.ts
@@ -7,20 +7,52 @@ export class ListNode {
         this.val = val === undefined ? 0 : val;
         this.next = next === undefined ? null : next;
     }
+
+    /**
+     * Turn a group of numbers to ListNode object.
+     * @param nodes leetcode testcase
+     */
+    static ListNodeBuilder(nodes: number[]): ListNode {
+        let dHead = new ListNode(0);
+        let tail = dHead;
+        nodes.forEach((value) => {
+            tail.next = new ListNode(value);
+            tail = tail.next;
+        });
+        return dHead.next;
+    }
 }
 
-/**
- * Turn a group of numbers to ListNode object.
- * @param nodes leetcode testcase
- */
-export function ListNodeBuilder(nodes: number[]): ListNode {
-    let dHead = new ListNode(0);
-    let tail = dHead;
-    nodes.forEach((value) => {
-        tail.next = new ListNode(value);
-        tail = tail.next;
-    });
-    return dHead.next;
+export class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+
+    /**
+     * Turn a group of numbers to TreeNode object.
+     * @param nodes leetcode_testcase
+     */
+    static TreeNodeBuilder(nodes: number[]): TreeNode {
+        let root = new TreeNode(nodes.shift());
+        let queue: (TreeNode | null)[] = [root];
+
+        while (queue.length > 0) {
+            let cur = queue.shift();
+            if (cur) {
+                let left = nodes.shift();
+                let right = nodes.shift();
+                if (left) cur.left = new TreeNode(left);
+                if (right) cur.right = new TreeNode(right);
+                queue.push(cur.left, cur.right);
+            }
+        }
+        return root;
+    }
 }
 
 export namespace Utils {
