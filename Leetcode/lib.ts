89df31861123ab0c84dce4439350b42a664d7bb2
miaachan <kimyeungg@outlook.com>
2021-01-18 22:05:21 +0800

    lib: heap

--- Leetcode/lib.ts
+++ Leetcode/lib.ts
@@ -21,6 +21,15 @@ export class ListNode {
         });
         return dHead.next;
     }
+
+    static print(node: ListNode): void {
+        let str = "";
+        while (node) {
+            str += `${node.val} -> `;
+            node = node.next;
+        }
+        console.log(str + "NULL");
+    }
 }
 
 export class TreeNode {
@@ -52,6 +61,76 @@ export class TreeNode {
             }
         }
         return root;
+    }
+}
+
+export class Heap<T> {
+    private _heap = [];
+
+    protected parent_indx = (n): number => Math.floor((n - 1) / 2);
+    protected left_child_indx = (n: number): number => 2 * n + 1;
+    protected right_child_indx = (n: number): number => 2 * n + 2;
+    protected swap = (a, b): void => {
+        [this._heap[a], this._heap[b]] = [this._heap[b], this._heap[a]];
+    };
+    get size() {
+        return this._heap.length;
+    }
+
+    bubble_up() {
+        let index = this._heap.length - 1;
+        while (index > 0) {
+            let parent_index = this.parent_indx(index);
+            if (this._heap[index] > this._heap[parent_index]) {
+                this.swap(index, parent_index);
+                index = parent_index;
+                continue;
+            }
+            break;
+        }
+    }
+
+    // adjust element after popping the max value
+    bubble_down() {
+        let parent_index = 0;
+        while (true) {
+            let parent: number = this._heap[parent_index];
+            let left_index = this.left_child_indx(parent_index);
+            let right_index = this.right_child_indx(parent_index);
+
+            let left_child = this._heap[left_index];
+            let right_child = this._heap[right_index];
+
+            let to_be_swap = undefined;
+
+            // if there is any child that > parent; swap(child, parent)
+            // if both left & right are greater than parent, pick the greatest one.
+            // pick right if right > left; otherwise, pick left.
+            if (left_child && left_child > parent) to_be_swap = left_index;
+            if (right_child && right_child > parent && right_child > left_child)
+                to_be_swap = right_index;
+
+            // No children are greater than the parent, mission completed.
+            if (!to_be_swap) break;
+            this.swap(parent_index, to_be_swap);
+            parent_index = to_be_swap;
+        }
+    }
+
+    peek(): number | undefined {
+        return this._heap[0];
+    }
+
+    pop(): void {
+        this.swap(0, this.size - 1);
+        let pop_value = this._heap.pop();
+        this.bubble_down();
+        return pop_value;
+    }
+
+    insert(node): void {
+        this._heap.push(node);
+        this.bubble_up();
     }
 }
 
